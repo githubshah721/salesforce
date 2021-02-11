a8c03ac4d3ebe91e9f00cf2b705199f8bf8c5cf3
shaid-hussain <shaid@gappsexperts.com>
2021-02-11 15:02:10 +0530

    3:00

--- src/main/java/com/salesforce/tooling/service/impl/ToolingServiceImpl.java
+++ src/main/java/com/salesforce/tooling/service/impl/ToolingServiceImpl.java
@@ -2,36 +2,23 @@ package com.salesforce.tooling.service.impl;
 
 
 import com.google.gson.JsonObject;
-import com.salesforce.tooling.client.SalesForceToolingServiceGenerator;
 import com.salesforce.tooling.client.SalesServicePP;
 import com.salesforce.tooling.client.ToolingSObjects;
 import com.salesforce.tooling.service.ToolingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import retrofit2.Call;
 import retrofit2.Response;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Service
 public class ToolingServiceImpl implements ToolingService {
 
-    private RestTemplate restTemplate = new RestTemplate();
-    ;
-
     @Autowired
     private SalesServicePP salesServicePP;
-
 
     @Override
     public JsonObject getToolingSObjects() {
@@ -61,9 +48,5 @@ public class ToolingServiceImpl implements ToolingService {
 
         }
         return null;
-    }
-
-    private void de() {
-
     }
 }
